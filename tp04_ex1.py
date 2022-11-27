cf536fdf0cf535a42a5503d26e51baa31a417d43
JeromeHumbert <54804629+JeromeHumbert@users.noreply.github.com>
2022-11-27 14:45:09 +0100

    Add files via upload

---
+++ tp04_ex1.py
@@ -0,0 +1,4 @@
+
+
+if __name__ == "__main__":
+    liste_chiffres : list = [8,2,4,6,9,1,3]
---
+++ tp04_ex2.py
@@ -0,0 +1,7 @@
+
+
+
+
+if __name__ == "__main__":
+    liste_couleur: list = ["Rouge", "Vert", "Bleu", "Orange", "Jaune", "Brun"]
+
